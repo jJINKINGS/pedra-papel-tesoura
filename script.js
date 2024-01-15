2a972b82c8605a730bba80c0142450905fa1a5b6
jJINKINGS <joaomatheusjinkings@gmail.com>
2024-01-15 16:46:46 -0300

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+function playRockPaperScissor (player1, player2) {
+
+    if((player1 === "Pedra" && player2 === "Tesoura")||(player1 === "Papel" && player2 === "Pedra")||(player1 === "Tesoura" && player2 === "Papel")) {
+        return "Jogador 1 venceu!";
+    }
+    else if((player1 === "Pedra" && player2 === "Papel")||(player1 === "Papel" && player2 === "Tesoura")||(player1 === "Tesoura" && player2 === "Pedra")) {
+        return "Jogador 2 venceu!";
+    }
+
+    return "Empate!";
+}
+
+    
+
+    
+    
+
+
